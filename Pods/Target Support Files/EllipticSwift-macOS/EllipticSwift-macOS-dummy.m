3d56bfd65728ca6ceb8dd3c85d7f32b537846c78
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-10-12 18:06:48 +0300

    commit files from pods

---
+++ Pods/Target Support Files/EllipticSwift-macOS/EllipticSwift-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EllipticSwift_macOS : NSObject
+@end
+@implementation PodsDummy_EllipticSwift_macOS
+@end
--- Pods/Target Support Files/EllipticSwift/EllipticSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EllipticSwift : NSObject
-@end
-@implementation PodsDummy_EllipticSwift
-@end
